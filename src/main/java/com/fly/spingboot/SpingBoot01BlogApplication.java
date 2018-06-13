0af4039f6fc550ea42889ddf9b4bfcba21f12efe
fly <zgqtsyx@163.com>
2018-06-13 22:43:55 +0800

    Initial commit

---
+++ src/main/java/com/fly/spingboot/SpingBoot01BlogApplication.java
@@ -0,0 +1,12 @@
+package com.fly.spingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBoot01BlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingBoot01BlogApplication.class, args);
+    }
+}
---
+++ src/test/java/com/fly/spingboot/SpingBoot01BlogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fly.spingboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpingBoot01BlogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
